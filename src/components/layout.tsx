b1d58f16489e3f05cdfcafe1017b3d31f4ebd717
applejam <46771211+applejam9029@users.noreply.github.com>
2020-12-24 16:29:24 +0900

    add transition in layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,8 @@ import i18n from "i18next"
 import en from "../lang/en"
 import ja from "../lang/ja"
 
+import { useTransition } from "../hooks/transition"
+
 import Header from "./header"
 import Footer from "./footer"
 
@@ -23,12 +25,21 @@ export const Layout: React.FC<{ lang: string; isIndex?: boolean }> = ({
 }) => {
   i18n.changeLanguage(lang)
 
+  const animationTarget = useTransition({
+    enterFrom: "scale-x-0",
+    enterTo: "scale-x-100",
+  })
+
   return (
     <div className="text-gray-1000 min-h-full flex flex-col divide-y dark:divide-gray-500">
       <header className="p-2 bg-gray-800 dark:bg-gray-900 shadow flex-grow-0">
         <Header lang={lang} isIndex={!!isIndex} />
       </header>
-      <div className="flex-grow-0 bg-gray-0 dark:bg-gray-800">{children}</div>
+      <div
+        className={`flex-grow-0 bg-gray-0 dark:bg-gray-800 transform transition-transform ${animationTarget}`}
+      >
+        {children}
+      </div>
       <footer className="bg-gray-800 dark:bg-gray-900 shadow flex-grow">
         <Footer />
       </footer>
